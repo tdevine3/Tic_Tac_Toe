8bdafed06e12bb4f35f21fd9697826ae328989b5
Teddy\Toby <tdevine3@mix.wvu.edu>
2017-08-15 19:22:25 -0400

    Testing buttons

--- app/src/main/java/toby/tic_tac_toe/MainActivity.java
+++ app/src/main/java/toby/tic_tac_toe/MainActivity.java
@@ -1,7 +1,11 @@
 package toby.tic_tac_toe;
 
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button)findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView tv = (TextView) findViewById(R.id.WinningMessage);
+                tv.setVisibility(View.VISIBLE);
+                tv.setText(R.string.winnerx);
+            }
+
+
+        });
+
     }
 }
--- app/src/main/java/toby/tic_tac_toe/Memory.java
+++
@@ -1,7 +0,0 @@
-package toby.tic_tac_toe;
-
-class Memory extends AI {
-	int bestMove;
-	int bestScore; 
-	int[] scorelist;
-}
